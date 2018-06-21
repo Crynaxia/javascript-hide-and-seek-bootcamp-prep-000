db7da66928a2c2c1364fc6d6024c0105f65bed6c
robert.zacchia@gmail.com <robert.zacchia@gmail.com>
2018-06-21 17:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,31 @@ function nestedTarget()
   return document.querySelector('#nested .target');
 }
 
+function increaseRankBy(n)
+{
+
+ const rankedLists = document.querySelectorAll('.ranked-list')
+ 
+   for (let i = 0, l = rankedLists.length; i < l; i++) {
+     let children = rankedLists[i].children
+ 
+     for (let j = 0, k = children.length; j < k; j++) {
+       children[j].innerHTML = parseInt(children[j].innerHTML) + n
+     }
+   } 
+  
+  
+}
 
 
 function deepestChild() {
-  var grandNode = document.querySelector('#grand-node');
-  var deepestNode = grandNode.children[0];
-  for (var i = 0; !deepestNode.children[i]; i) {
-    deepestNode = deepestNode.children[0]
-  }
-  return deepestNode;
-}
+   let node = document.getElementById('grand-node')
+   let nextNode = node.children[0]
+ 
+   while (nextNode) {
+     node = nextNode
+     nextNode = node.children[0]
+   }
+ 
+   return node
+ }
